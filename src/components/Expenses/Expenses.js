91df7741bb01e3dd157509342b5f24d4acd02e6b
Nirmal Kumar Mali <officialnirmal2060@gmail.com>
2025-04-18 17:01:14 +0545

    React Basics & Working With Components

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+    return (
+        <Card className="expenses">
+            {props.items.map((expense) => (
+                <ExpenseItem key={expense.id} expense={expense} />
+            ))}
+        </Card>
+    );
+}
+
+export default Expenses;
